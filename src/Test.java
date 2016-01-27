eef49be699199878430288c294e49814e0818214
Alan <smithalan2@gmail.com>
2016-01-27 17:45:01 +0000

    Commented example

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,19 @@
-
 public class Test {
-    
-    
-    
     public static void main(String[]args){
+        System.out.println("This is a simple example of using BCrypt to hash and verify passwords\n\n");
+        System.out.println("First we generate some salt, then hash our password with that salt.");
+        System.out.println("Then we use the checkpw function to compare some text (our password) against our hashed password");
+        System.out.println("The checkpw function returns true or false\n\n");
+        //Create new BCrypt object
         BCrypt b = new BCrypt();
-        
+        //Generate some salt to hash the password
         String salt = BCrypt.gensalt();
-        String password = "alansmithiscool";
-        
-        String hash = BCrypt.hashpw(password, salt);
-        
-        System.out.println(BCrypt.checkpw(password, salt));
-        
+        //Hash the password using the salt
+        String hash = BCrypt.hashpw("mypassword", salt);
+        //Is the password password?
+        System.out.println("Is the password = password: "+BCrypt.checkpw("password", hash));
+        //Is the password mypassword?
+        System.out.println("Is the password = mypassword: "+BCrypt.checkpw("mypassword", hash)); 
     }
     
 }
